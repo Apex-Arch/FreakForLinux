a366458d19174081218a5a7b2ce2ad61bcd7e49d
Apex-Arch <inowicka82@gmail.com>
2025-06-11 07:54:28 +0200

    Freak.py

--- Freak.py
+++ Freak.py
@@ -8,7 +8,7 @@ while True:
     os.system("sudo apt update && sudo apt install -y python3")
     os.system("sudo git clone https://github.com/palahsu/DDoS-Ripper.git")
     os.system("clear")
-    full_path = "/DDoS-Ripper"
+    full_path = "FreakForLinux/DDoS-Ripper"
     os.chdir(full_path)
     print("███████╗██████╗░███████╗░█████╗░██╗░░██╗")
     print("██╔════╝██╔══██╗██╔════╝██╔══██╗██║░██╔╝")
